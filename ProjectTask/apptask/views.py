e2927bd68e4078bc77e0c7874498d8da9cd50ca2
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-02 23:59:50 +0530

    commit

---
+++ ProjectTask/apptask/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(req):
+    return render(req,'home.html')
+
+def designs(req):
+    return render(req,'designs.html')
+
+def about(req):
+    return render(req,'about.html')
+
+def services(req):
+    return render(req,'services.html')
+
+def login(req):
+    return render(req,'login.html')
+
+def register(req):
+    return render(req,'register.html')
