e979058c2d661f7c51519d0e7411f39d38c22fe0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-09-01 22:18:24 +0530

    commit

--- ProjectTask/ProjectTask/settings.py
+++ ProjectTask/ProjectTask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qywvdhs3kf)6b2#uu!old*(5vui7giqle(n072b6a6_!n78zup'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
